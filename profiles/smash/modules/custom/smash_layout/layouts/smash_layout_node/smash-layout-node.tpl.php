2b1a6a920213df2e415069bffa1a96a578d258c7
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-10-04 00:12:06 +1000

    Added layout

---
+++ profiles/smash/modules/custom/smash_layout/layouts/smash_layout_node/smash-layout-node.tpl.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @file
+ * Custom TTC display suite template
+ */
+
+// Reuse with panels!
+$pfx = 'smash_layout_node';
+if (isset($content)) {
+  foreach ($content as $key => $item) {
+    $name = $pfx.$key;
+    ${$name} = $item;
+    
+    if (!isset(${$name.'_classes'})) {
+      ${$name.'_classes'} = '';
+    }
+  }
+}
+if (!isset($title_suffix)) {
+  $title_suffix = array();
+}
+else {
+  if (isset($title_suffix['contextual_links'])) {
+    $admin_links = $title_suffix['contextual_links'];
+  }
+}
+
+?>
+
+<div class="<?php print $classes;?> clearfix">
+
+  <?php if (isset($admin_links)): ?>
+  <?php print render($admin_links); ?>
+  <?php endif; ?>
+  
+  <div class="content-main-area clearfix">
+    <div class="content-main-area-inner">
+
+	  <?php if ($smash_layout_node_main) { ?>
+		<div class="prefaced-node-content <?php print $smash_layout_node_main_classes; ?>">
+		  <?php print $smash_layout_node_main; ?>
+		</div>
+	  <?php } ?>
+	
+	  <?php if ($smash_layout_node_sidebar) { ?>
+		<div class="prefaced-node-sidebar <?php print $smash_layout_node_sidebar_classes; ?>">
+		  <?php print $smash_layout_node_sidebar; ?>
+		</div>
+	  <?php } ?>
+    </div>  
+  </div>
+  
+</div>
