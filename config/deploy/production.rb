e6db36175f1d0fdedcd3fc912716cd681520877b
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-12-20 00:21:11 +1100

    Added deploy files

---
+++ config/deploy/production.rb
@@ -0,0 +1,2 @@
+server 'linode.smash.org.au', :app, :web, :primary => true
+set :deploy_to, '/var/www/smash.org.au/www'
---
+++ config/deploy/staging.rb
@@ -0,0 +1,2 @@
+server 'linode.smash.org.au', :app, :web, :primary => true
+set :deploy_to, '/var/www/staging.smash.org.au/www'
