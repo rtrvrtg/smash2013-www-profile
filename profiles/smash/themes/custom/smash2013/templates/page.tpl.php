4047f5215ab5cfc61fea86246a4a4a4d7170135d
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-12-23 16:11:37 +1100

    Style updates

--- profiles/smash/themes/custom/smash2013/templates/page.tpl.php
+++ profiles/smash/themes/custom/smash2013/templates/page.tpl.php
@@ -75,21 +75,17 @@
   <header id="header" role="banner">
 
     <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
-        <?php if ($site_name): ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
-        <?php endif; ?>
-
+      <hgroup id="logo-region">
+        <h1 id="site-name">
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <span class="element-hidden"><?php print $site_name; ?></span>
+        </a>
+        </h1>
         <?php if ($site_slogan): ?>
           <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
-      </hgroup><!-- /#name-and-slogan -->
+      </hgroup>
     <?php endif; ?>
 
     <?php if ($secondary_menu): ?>
