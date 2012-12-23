fb43efda23bf17431ee41ea86b37b51cde96111b
Geoffrey Roberts <g.roberts@blackicemedia.com>
2012-12-23 18:22:03 +1100

    More tpl updates

--- profiles/smash/themes/custom/smash2013/templates/page.tpl.php
+++ profiles/smash/themes/custom/smash2013/templates/page.tpl.php
@@ -87,23 +87,6 @@
         <?php endif; ?>
       </hgroup>
     <?php endif; ?>
-
-    <?php if ($secondary_menu): ?>
-      <nav id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => $secondary_menu_heading,
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </nav>
-    <?php endif; ?>
-
     <?php print render($page['header']); ?>
 
   </header>
