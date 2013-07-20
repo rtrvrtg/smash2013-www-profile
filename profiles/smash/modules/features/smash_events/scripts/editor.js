63efb7fd5beccb7577562271a70d6c245eb358be
Geoffrey Roberts <g.roberts@blackicemedia.com>
2013-07-20 14:30:43 +1000

    Trying to fix date selection weirdness

---
+++ profiles/smash/modules/features/smash_events/scripts/editor.js
@@ -0,0 +1,27 @@
+(function($){
+
+$(document).ready(function(){
+
+	var fields = $('input[name="field_event_date_time[und][0][value][date]"], input[name="field_event_date_time[und][0][value2][date]"]');
+
+	fields.parent().hide();
+
+	var updateField = function(){
+		if ($(this).val().indexOf('-') >= 0) {
+			var parts = $(this).val().split('-');
+			var newVal = [
+				parts[1],
+				parts[2],
+				parts[0]
+			].join('/');
+			$(this).val(newVal);
+		}
+	};
+
+	fields.change(updateField);
+	fields.each(updateField);
+
+});
+
+
+})(jQuery);
